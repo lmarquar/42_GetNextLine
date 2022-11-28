63f235aa3d948fd7f700f20726683603570e4900
lmarquar <leon.marquardt@gmx.de>
2022-11-28 23:43:05 +0100

    initializing repo

---
+++ get_next_line_bonus.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmarquar <lmarquar@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/04 17:53:42 by lmarquar          #+#    #+#             */
+/*   Updated: 2021/07/14 10:46:51 by lmarquar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+
+char	*get_next_line_bonus(int fd);
+void	*ft_calloc(size_t nmemb, size_t size);
+
+#endif
